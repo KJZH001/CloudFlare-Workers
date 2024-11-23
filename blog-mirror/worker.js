9e63c755795e0b9d0c0fe62ec9c9c39e6c0629f0
KJZH001 <2802007619@qq.com>
2024-11-23 13:09:23 +0800

    博客域名301

---
+++ blog-mirror/worker.js
@@ -0,0 +1,12 @@
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request))
+  })
+  
+  async function handleRequest(request) {
+    const url = new URL(request.url)
+    // 构建新的 URL，替换主机名为 blog.moeworld.tech
+    url.hostname = 'blog.moeworld.tech'
+    // 返回 301 重定向响应
+    return Response.redirect(url.toString(), 301)
+  }
+  
